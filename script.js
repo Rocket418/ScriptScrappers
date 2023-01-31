0280db707918209281823aa4f00328fe8e4c3a46
jeronimo fernandez <jeronimo2000.jf@gmail.com>
2023-01-31 13:59:38 +0100

    se sacan las letras de las respuestas con expresiones regulares y replace de mala forma

--- script.js
+++ script.js
@@ -17,13 +17,14 @@ async function reorganize(){
         intermedio:[],
         alto:[],
     }
+    const fixedAnswer = [
+        
+    ]
     if(infoScrapper.length===4){
         for (infoscrap of infoScrapper){
             scraps.push(infoscrap)
-            if(scraps[3]){
-                if(!scraps[3]['SIMULACROS 2022 EN PROCESO DE ACTUALIZACIÓN'].length){
-                    scraps.pop()
-                }
+            if(scraps[3] && !scraps[3]['SIMULACROS 2022 EN PROCESO DE ACTUALIZACIÓN'].length){
+                scraps.pop()
             }
         }
         for (scrap of scraps){
@@ -31,24 +32,47 @@ async function reorganize(){
                 for (themes of scrap[topic]){
                     for (theme in themes){
                         for(let i = 0; i < themes[theme].length; i++){
-                                let indexof = Object.keys(themes[theme][i])
                                 const test = themes[theme][i]
+                                let indexof = Object.keys(test)
                                 if(indexof[0].includes('INTRO') || indexof[0].includes('NIVEL 1')){
-                                    Category.basico.push(test)
+                                    Category.basico.push(test) 
                                 } else if(indexof[0].includes('NIVEL 2') || indexof[0].includes('NIVEL 3')){
                                     Category.intermedio.push(test)
                                 } else if(indexof[0].includes('NIVEL 4')){
                                     Category.alto.push(test)
                                 }
-                                for ()
+                                for(items in test){
+                                    for(item of test[items]){
+                                            if(item.question && item.answers && item.rightanswer){
+                                                /* const cleanText = strInputCode.replace(/<\/?[^>]+(>|$)/g, "") */
+                                                const answerObject = {
+                                                question: '',
+                                                answers: [],
+                                                rightAnswer: '',
+                                                feedback: '',
+                                            }   
+                                            answerObject.question = item.question.replace(/<\/?[^>]+(>|$)/g, "")
+                                            answerObject.rightAnswer = item.rightanswer.replace(/<\/?[^>]+(>|$)/g, "")
+                                            answerObject.feedback = item.feedBack.replace(/<\/?[^>]+(>|$)/g, "").replaceAll('\n', '').replaceAll('&nbsp;', '')
+                                            for (answer of item.answers){
+                                                answerObject.answers.push(answer.replaceAll('\n', '').replaceAll('a.', '').replaceAll('b.', '').replaceAll('c.', ''))
+                                            }
+                                            console.log(answerObject)
+                                        }
+                                        break
+                                    }
+                                    break
+                                }
+                                
+                                break
                         }
-                        
+                        break
                     }
-                    
+                    break
                 }
-                
+                break
             }
-            
+            break
         }
     }
     /* console.log(Category) */
