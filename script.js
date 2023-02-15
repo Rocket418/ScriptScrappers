b7df1aba91067df56ada4e4686c5bd6fe205d82e
jeronimo fernandez <jeronimo2000.jf@gmail.com>
2023-02-15 14:57:22 +0100

    codigo reestructurado

--- script.js
+++ script.js
@@ -1,47 +1,42 @@
 const fs = require('fs');
 
 
-async function call(police)  {
+async function call(police) {
     return fetch(police)
         .then((response) => response.json())
-        .then((scrapped) => {  return scrapped  })
+        .then((scrapped) => { return scrapped })
         .catch((error) => console.log('Error al hacer el fetch', error));
 };
 
 
 
-async function reorganize()  {
+async function reorganize() {
     const dePol = 'http://localhost:5000/dePol';
     const ofiPol = 'http://localhost:5000/ofiPol';
-    const infoScrapper = await call(ofiPol)
+    const used = ofiPol;
+    const infoScrapper = await call(used)
     const scraps = []
-    let counter  = 0;
+    let counter = 0;
     let answerCounter = 0;
     const Category = {
-        unsorted2: [],
         unsorted: [],
-        unsorted: [],
-        basico:  [],
-        intermedio:  [],
-        alto:  [],
+        basico: [],
+        intermedio: [],
+        alto: [],
     }
-    const fixedAnswers = []
-    if (infoScrapper.length === 4) {
-        for (infoscrap of infoScrapper) {
-            scraps.push(infoscrap)
-            if  (scraps[3] && !scraps[3]['SIMULACROS 2022 EN PROCESO DE ACTUALIZACIÓN'].length)  {
-                scraps.pop()
-            }
+    let fixedAnswers = []
+    for (infoScrap of infoScrapper) {
+        scraps.push(infoScrap)
+        if (scraps[3] && !scraps[3]['SIMULACROS 2022 EN PROCESO DE ACTUALIZACIÓN'].length) {
+            scraps.pop()
         }
-        for (scrap of scraps) {
-            for (topic in scrap) {
-                for (themes of scrap[topic]) {
-                    for (theme in themes) {
-                        for (let i = 0; i < themes[theme].length; i++) {
-                            const test = themes[theme][i]
-                            let indexof = Object.keys(test)
-                            for (items in test) {
-                                for (item of test[items]) {
+        for (scrapThemes in infoScrap) {
+            infoScrap[scrapThemes].map(function (x) {
+                for (difficulty in x) {
+                    for (topic of x[difficulty]) {
+                        if (used === dePol) {
+                            for (test in topic) {
+                                for (questionObject of topic[test]) {
                                     counter = counter + 1
                                     const answerObject = {
                                         question: '',
@@ -50,101 +45,68 @@ async function reorganize()  {
                                         feedback: '',
                                         id: counter
                                     }
-
-                                    answerObject.question = item.question.replace(/<\/?[^>]+(>|$)/g, "")
-                                    answerObject.feedback = item.feedBack.replace(/<\/?[^>]+(>|$)/g, "").replaceAll('\n', '').replaceAll('&nbsp;', '')
-                                    for (answer of item.answers) {
-
+                                    answerObject.answers = []
+                                    let realQuestion = questionObject.question.replace(/<\/?[^>]+(>|$)/g, "")
+                                    answerObject.question = realQuestion
+                                    answerObject.feedback = questionObject.feedBack.replace(/<\/?[^>]+(>|$)/g, "").replaceAll('\n', '').replaceAll('&nbsp;', '')
+                                    for (answer of questionObject.answers) {
                                         answerCounter = answerCounter + 1;
                                         const cleanAnswer = {
                                             id: answerCounter,
-                                            answer: answer.replaceAll('&nbsp;', '').replace(/<\/?[^>]+(>|$)/g, "").replaceAll('\n', '').split('.',)
+                                            goodAnswer: answer.replaceAll('&nbsp;', '').replace(/<\/?[^>]+(>|$)/g, "").replaceAll('\n', '').split('.')
                                         }
+                                        
                                         answerObject.answers.push(cleanAnswer)
-
                                     }
                                     function correctAnswer() {
                                         for (string of answerObject.answers) {
-                                            if (!!item.rightanswer.toString().includes(string.answer[1].toString())) {
+                                            if (!!questionObject.rightanswer.toString().includes(string.goodAnswer[1])) {
                                                 return string.id
                                             }
                                         }
                                     }
                                     answerObject.rightAnswer = correctAnswer()
+                                    fixedAnswers = []
                                     fixedAnswers.push({
-                                        Origin: items,
+                                        Origin: test,
                                         Answer: answerObject
                                     }
                                     )
+                                    
                                 }
+                                if (test.includes('INTRO') || test.includes('NIVEL 1')) {
+                                    for (fixedAnswer of fixedAnswers){
+                                        if (test === fixedAnswer.Origin){
+                                            let index = {
+                                                Origin: difficulty,
+                                                Preguntas: fixedAnswer.Answer
+                                            }
+                                            Category.basico.push(index)    
+                                        }
+                                    }
+                                } else if (test.includes('NIVEL 2') || test.includes('NIVEL 3')) {
+                                    for (fixedAnswer of fixedAnswers) {
+                                        if (test === fixedAnswer.Origin) {
+                                            let index = {
+                                                Origin: test,
+                                                Preguntas: fixedAnswer.Answer
+                                            }
+                                            Category.intermedio.push(index)
+                                        }
+                                    }
+                                } else if (test.includes('NIVEL 4')) {
+                                    for (fixedAnswer of fixedAnswers) {
+                                        if (test === fixedAnswer.Origin) {
+                                            let index = {
+                                                Origin: test,
+                                                Preguntas: fixedAnswer.Answer
+                                            }
+                                            Category.alto.push(index)
+                                        }
+                                    }
+                                }   
                             }
-                            if (indexof[0].includes('INTRO') || indexof[0].includes('NIVEL 1')) {
-                                for (fixedAnswer in fixedAnswers) {
-                                    if (indexof[0] === fixedAnswers[fixedAnswer].Origin) {
-                                        let index = {
-                                            Origin: indexof,
-                                            Preguntas: fixedAnswers[fixedAnswer].Answer
-                                        }
-                                        Category.basico.push(index)
-                                    }
-                                }
-                            } else if (indexof[0].includes('NIVEL 2') || indexof[0].includes('NIVEL 3')) {
-                                for (fixedAnswer in fixedAnswers) {
-                                    if (indexof[0] === fixedAnswers[fixedAnswer].Origin) {
-                                        let index = {
-                                            Origin: indexof,
-                                            Preguntas: fixedAnswers[fixedAnswer].Answer
-                                        }
-                                        Category.intermedio.push(index)
-                                    }
-                                }
-
-                            } else if (indexof[0].includes('NIVEL 4')) {
-                                for (fixedAnswer in fixedAnswers) {
-                                    if (indexof[0] === fixedAnswers[fixedAnswer].Origin) {
-                                        let index = {
-                                            Origin: indexof,
-                                            Preguntas: []
-                                        }
-                                        index.Preguntas.push(fixedAnswers[fixedAnswer].Answer)
-                                        Category.alto.push(index)
-                                    }
-                                }
-
-                            } else {
-                                for (fixedAnswer in fixedAnswers) {
-                                    if (indexof[0] === fixedAnswers[fixedAnswer].Origin) {
-                                        let index = {
-                                            Origin: indexof,
-                                            Preguntas: []
-                                        }
-                                        index.Preguntas.push(fixedAnswers[fixedAnswer].Answer)
-                                        Category.basico.push(index)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }            
-        const completeResponse = Category
-        fs.writeFile('reorganicedScrapperdePol.json', JSON.stringify(completeResponse), (err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('dePol file written successfully');
-            }
-        }
-        )
-    }
-}else if (infoScrapper.length === 3) {
-        for (infoScrap of infoScrapper) {
-            scraps.push(infoScrap)
-            for (scrapThemes in infoScrap) {
-                /*Titulo del tema*/
-                infoScrap[scrapThemes].map(function (x) {
-                    for (difficulty in x) {
-                        for (topic of x[difficulty]) {
+                        } else if(used === ofiPol) {
                             counter = counter + 1
                             const answerObject = {
                                 question: '',
@@ -160,88 +122,92 @@ async function reorganize()  {
                                 answerCounter = answerCounter + 1;
                                 const cleanAnswer = {
                                     id: answerCounter,
-                                    answer: answer.replaceAll('&nbsp;', '').replace(/<\/?[^>]+(>|$)/g, "").replaceAll('\n', '').split(') ',)
+                                    goodAnswer: answer.replaceAll('&nbsp;', '').replace(/<\/?[^>]+(>|$)/g, "").replaceAll('\n', '').split(') ',)
                                 }
                                 answerObject.answers.push(cleanAnswer)
                             }
-                            function correctAnswer() {
+                            function correctAnswer2() {
                                 for (string of answerObject.answers) {
-                                    if (!!topic.rightanswer.toString().includes(string.answer[0])) {
+                                    if (!!topic.rightanswer.toString().includes(string.goodAnswer[0])) {
                                         return string.id
                                     }
                                 }
                             }
-                            answerObject.rightAnswer = correctAnswer()
+                            answerObject.rightAnswer = correctAnswer2()
+                            fixedAnswers = []
                             fixedAnswers.push({
                                 Origin: difficulty,
                                 Answer: answerObject
-                            }
-                            )
+                            })
                         }
-                            if (difficulty.includes('Fácil')) {
-                                console.log('soy facil')
-                                for (fixedAnswer in fixedAnswers) {
-                                    if (difficulty === fixedAnswers[fixedAnswer].Origin) {
-                                        let index = {
-                                            Origin: scrapThemes,
-                                            Preguntas: fixedAnswers[fixedAnswer].Answer
-                                        }
-                                        Category.basico.push(index)
+                        if (difficulty.includes('Facil') || difficulty.includes('Fácil')) {
+                            for (fixedAnswer of fixedAnswers) {
+                                if (difficulty === fixedAnswer.Origin) {
+                                    let index = {
+                                        Origin: scrapThemes,
+                                        Preguntas: fixedAnswer.Answer
                                     }
-                                }
-                            } else if (difficulty.includes('Normal')) {
-                                for (fixedAnswer in fixedAnswers) {
-                                    if (difficulty === fixedAnswers[fixedAnswer].Origin) {
-                                        let index = {
-                                            Origin: scrapThemes,
-                                            Preguntas: fixedAnswers[fixedAnswer].Answer
-                                        }
-                                        Category.intermedio.push(index)
-                                    }
-                                }
-                                console.log(fixedAnswers[fixedAnswer].Origin)
-                                console.log(fixedAnswers[fixedAnswer].Answer)
-                            } else if (difficulty.includes('Dificil') || difficulty.includes('Difícil')) {
-                                console.log('soy dificil')
-                                for (fixedAnswer in fixedAnswers) {
-                                    if (difficulty === fixedAnswers[fixedAnswer].Origin) {
-                                        let index = {
-                                            Origin: scrapThemes,
-                                            Preguntas: fixedAnswers[fixedAnswer].Answer
-                                        }
-                                        Category.alto.push(index)
-                                    }
-                                }
-                            } else {
-                                console.log('soy indefinido')
-                                for (fixedAnswer in fixedAnswers) {
-                                    if (difficulty === fixedAnswers[fixedAnswer].Origin) {
-                                        let index = {
-                                            Origin: difficulty,
-                                            Preguntas: fixedAnswers[fixedAnswer].Answer
-                                        }
-                                        Category.unsorted.push(index)
-                                    }
+                                    Category.basico.push(index)
                                 }
                             }
+                        } else if (difficulty.includes('Normal')) {
+                            for (fixedAnswer of fixedAnswers) {
+                                if (difficulty === fixedAnswer.Origin) {
+                                    let index = {
+                                        Origin: scrapThemes,
+                                        Preguntas: fixedAnswer.Answer
+                                    }
+                                    Category.intermedio.push(index)
+                                }
+                            }
+                        } else if (difficulty.includes('Dificil') || difficulty.includes('Difícil')) {
+                            for (fixedAnswer of fixedAnswers) {
+                                if (difficulty === fixedAnswer.Origin) {
+                                    let index = {
+                                        Origin: scrapThemes,
+                                        Preguntas: fixedAnswer.Answer
+                                    }
+                                    Category.alto.push(index)
+                                }
+                            }
+                        } else {
+                            for (fixedAnswer of fixedAnswers) {
+                                if (difficulty === fixedAnswer.Origin) {
+                                    let index = {
+                                        Origin: difficulty,
+                                        Preguntas: fixedAnswer.Answer
+                                    }
+                                    Category.unsorted.push(index)
+                                }
+                            }
+                        }
                     }
-                })
-            }
-            
+                }
+            })
         }
         
-        const completeResponse = Category
+    }
+    
+    const completeResponse = Category
+    if (used === ofiPol) {
         fs.writeFile('reorganicedScrapperofiPol.json', JSON.stringify(completeResponse), (err) => {
             if (err) {
                 console.log(err);
             } else {
                 console.log('ofiPol file written successfully');
             }
-        }
-        )
+        })
+    } else if (used === dePol) {
+        fs.writeFile('reorganicedScrapperdePol.json', JSON.stringify(completeResponse), (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('dePol file written successfully');
+            }
+        })
     }
-
 }
+
 
 
 reorganize()
